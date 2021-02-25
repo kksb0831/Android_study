067d62011f79197aa5f53ab79a243fb45f2bc695
kksb0832 <kksb0832@gmail.com>
2021-02-25 13:52:55 +0900

    Initial commit

---
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,171 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    // 채널 ID 생성
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    // 알림의 업데이트나 취소할 때 연결될 수 있도로 알림 ID 생성
+    private static final int NOTIFICATION_ID = 0;
+    // 사용자에게 알림을 전달할 NotificationManager 객체 생성
+    private NotificationManager mNotifyManager;
+    // Broadcast 에 대하여 알림 작업을 나타내는 고유 상수 생성
+    private static final String ACTION_UPDATE_NOTIFICATION = "com.example.android.notification.ACTION_UPDATE_NOTIFICATION";
+    // 알림 종료시 전달할 ID
+    private static final String ACTION_DELETE_NOTIFICATION = "com.example.android.notification.ACTION_DELETE_NOTIFICATION";
+    // 버튼 객체 생성
+    private Button button_notify;
+    private Button button_update;
+    private Button button_cancel;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNotification();
+        }
+    };
+
+    private final BroadcastReceiver dReceiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            cancelNotification();
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+            }
+        });
+
+        button_update = findViewById(R.id.update);
+        button_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateNotification();
+            }
+        });
+
+        button_cancel = findViewById(R.id.cancel);
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelNotification();
+            }
+        });
+
+        registerReceiver(mReceiver, new IntentFilter(ACTION_UPDATE_NOTIFICATION));
+        registerReceiver(dReceiver, new IntentFilter(ACTION_DELETE_NOTIFICATION));
+        setNotificationButtonState(true, false,false);
+        createNotifacationChannel();
+
+    }
+
+
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        // PendingIntent 를 이용하여 알림을 클릭하면 앱이 뜨게 함
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("알림!!")
+                .setContentText("여긴 알림 메세지")
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setDeleteIntent(getDeleteIntent());
+    }
+
+
+    public void sendNotification() {
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.addAction(R.drawable.ic_update, getString(R.string.notify_button_update), updatePendingIntent);
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+        setNotificationButtonState(false,true, true);
+    }
+
+    public void updateNotification() {
+        Bitmap androidImage = BitmapFactory.decodeResource(getResources(), R.drawable.mascot_1);
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage).setBigContentTitle("알림 업데이트!!"));
+        mNotifyManager.notify(NOTIFICATION_ID,notifyBuilder.build());
+        setNotificationButtonState(false, false, true);
+    }
+
+    public void cancelNotification() {
+        mNotifyManager.cancel(NOTIFICATION_ID);
+        setNotificationButtonState(true, false, false);
+    }
+
+
+    public void createNotifacationChannel() {
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // 현재 빌드 버전이 Oreo 이상인지 판단
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,"Mascot Notification",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotifyManager.createNotificationChannel(notificationChannel);
+
+        }
+    }
+
+
+    void setNotificationButtonState(Boolean isNotifyEnabled, Boolean isUpdateEnabled, Boolean isCancelEnabled) {
+        button_notify.setEnabled(isNotifyEnabled);
+        button_update.setEnabled(isUpdateEnabled);
+        button_cancel.setEnabled(isCancelEnabled);
+    }
+
+    protected PendingIntent getDeleteIntent() {
+
+        Intent deleteIntent = new Intent(ACTION_DELETE_NOTIFICATION);
+
+        return PendingIntent.getBroadcast(this, NOTIFICATION_ID, deleteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+
+
+}
